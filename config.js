ed3614e8e21fa5ba9d659ac86d158b0c0ae078d4
aldolime900 <56601264+aldolime900@users.noreply.github.com>
2020-06-18 19:05:20 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN: 'NzEzMTk0MDkxMDQyODMyNDA1.XuuP2w.G5muOg1oFlAu0yW9L8CkLwdx_0I',//هون التوكن     
-    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4 ', 
-    prefix: '$',//هون برفكس البوت 
-    devs: ['604301201651138561']//هون ايديك 
-}
