db49eaec53df7f4d8dd67605fd02964d5c57df3c
aldolime900 <56601264+aldolime900@users.noreply.github.com>
2020-06-18 19:02:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzEzMTk0MDkxMDQyODMyNDA1.XuuO-g.-NIZMOqMyl8UoeENxlS4_C-xmBU',//هون التوكن     
+    TOKEN: 'NzEzMTk0MDkxMDQyODMyNDA1.XuuP2w.G5muOg1oFlAu0yW9L8CkLwdx_0I',//هون التوكن     
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4 ', 
     prefix: '$',//هون برفكس البوت 
     devs: ['604301201651138561']//هون ايديك 
